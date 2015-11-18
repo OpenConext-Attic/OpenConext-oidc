7c8e11162cbc5b9f373782dae414b8e321cdaefb
oharsta <oharsta@zilverline.com>
2015-11-18 09:17:03 +0100

    Ensure all comma separated comparisions against the actual multi value String ignore the ordering

--- oidc-server/src/test/java/oidc/model/FederatedUserInfoTest.java
+++ oidc-server/src/test/java/oidc/model/FederatedUserInfoTest.java
@@ -1,7 +1,6 @@
 package oidc.model;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import oidc.AbstractTestIntegration;
 import org.junit.Before;
@@ -11,7 +10,6 @@ import org.springframework.core.io.ClassPathResource;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
@@ -41,18 +39,29 @@ public class FederatedUserInfoTest {
     assertEquals("institution", jsonObject.getAsJsonPrimitive("schac_home_organization_type").getAsString());
     assertEquals("principal_name", jsonObject.getAsJsonPrimitive("edu_person_principal_name").getAsString());
     assertEquals("targeted_id", jsonObject.getAsJsonPrimitive("edu_person_targeted_id").getAsString());
-    assertEquals("student, faculty", jsonObject.getAsJsonPrimitive("edu_person_affiliation").getAsString());
-    assertEquals("student, faculty", jsonObject.getAsJsonPrimitive("edu_person_scoped_affiliation").getAsString());
+    assertCommaSeparatedStringEquality(
+        new String[]{"student", "faculty"},
+        jsonObject.getAsJsonPrimitive("edu_person_affiliation").getAsString()
+    );
+    assertCommaSeparatedStringEquality(
+        new String[]{"student", "faculty"},
+        jsonObject.getAsJsonPrimitive("edu_person_scoped_affiliation").getAsString()
+    );
     assertEquals("surfnet", jsonObject.getAsJsonPrimitive("is_member_of").getAsString());
-
-    //The ordering is not constant
-    Set<String> entitlementsExpected = new HashSet<>(Arrays.asList("http://xstor.com/contracts/HEd123","urn:mace:washington.edu:confocalMicroscope"));
-    String[] entitlementsActual = jsonObject.getAsJsonPrimitive("edu_person_entitlement").getAsString().split(", ");
-    assertEquals(entitlementsExpected, new HashSet<>(Arrays.asList(entitlementsActual) ));
-
-
+    assertCommaSeparatedStringEquality(
+        new String[]{"http://xstor.com/contracts/HEd123", "urn:mace:washington.edu:confocalMicroscope"},
+        jsonObject.getAsJsonPrimitive("edu_person_entitlement").getAsString()
+    );
     assertEquals("personal", jsonObject.getAsJsonPrimitive("schac_personal_unique_code").getAsString());
     assertEquals("uid2, uid1", jsonObject.getAsJsonPrimitive("uid").getAsString());
+
+  }
+
+  //The ordering is not constant
+  private void assertCommaSeparatedStringEquality(String[] expected, String actual) {
+    Set<String> expectedSet = new HashSet<>(Arrays.asList(expected));
+    Set<String> actualSet = new HashSet<>(Arrays.asList(actual.split(", ")));
+    assertEquals(expectedSet, actualSet);
 
   }
 }
