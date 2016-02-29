a44a24af0686510fa0088060a8b3ffecac023c2c
oharsta <oharsta@zilverline.com>
2016-02-29 11:27:19 +0100

    New SNAPSHOT version

--- start.sh
+++ start.sh
@@ -2,4 +2,5 @@
 # file for local development to start the application.
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd $SCRIPT_DIR/oidc-server
+export MAVEN_OPTS="-Xmx1024m -Xdebug -Xrunjdwp:transport=dt_socket,address=8787,server=y,suspend=n"
 mvn clean package jetty:run -Dspring.profiles.active="local"
