d2d480c360eacf9273772154e8826f6d06b95981
oharsta <oharsta@zilverline.com>
2017-06-18 14:14:06 +0200

    Ensure we use the RS256 algorithm consistently

--- oidc-server/src/main/java/oidc/security/OidcKeystoreGenerator.java
+++ oidc-server/src/main/java/oidc/security/OidcKeystoreGenerator.java
@@ -5,36 +5,44 @@ import net.minidev.json.JSONStyle;
 
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
+import java.security.Provider;
+import java.security.Security;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
+import java.util.TreeSet;
 
 public class OidcKeystoreGenerator {
 
-  public static void main(String[] args) {
-    try {
-      new OidcKeystoreGenerator().generate();
-    } catch (Exception e) {
-      throw new RuntimeException(e);
+    public static void main(String[] args) {
+        try {
+            new OidcKeystoreGenerator().generate();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
-  }
 
-  public void generate() throws Exception {
-    KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
-    kpg.initialize(2048);
-    KeyPair keyPair = kpg.generateKeyPair();
-    RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
-    RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
+    public void generate() throws Exception {
+        Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
 
-    com.nimbusds.jose.jwk.RSAKey build = new com.nimbusds.jose.jwk.RSAKey.Builder(publicKey)
-        .privateKey(privateKey)
-        .algorithm(new Algorithm("RSA"))
-        .keyID("oidc")
-        .build();
-    String json = build.toJSONObject().toJSONString(JSONStyle.NO_COMPRESS);
+        KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA", "BC");
+        kpg.initialize(2048);
+        KeyPair keyPair = kpg.generateKeyPair();
+        RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
+        RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
 
-    System.out.println("\nCopy the json below to the secrets file for the target environment under the key oidc_server_oidc_keystore_jwks_json\n" +
-        "This will ensure it ends up on the classpath in a file name oidc.keystore.jwks.json\n");
-    System.out.println(json);
-    System.out.println("\n");
-  }
+        com.nimbusds.jose.jwk.RSAKey build = new com.nimbusds.jose.jwk.RSAKey.Builder(publicKey)
+            .privateKey(privateKey)
+            .algorithm(new Algorithm("RS256"))
+            .keyID("oidc")
+            .build();
+
+        String json = build.toJSONObject().toJSONString(JSONStyle.NO_COMPRESS);
+
+        System.out.println("\nCopy the json below to the secrets file for the target environment under the key oidc_server_oidc_keystore_jwks_json\n" +
+            "This will ensure it ends up on the classpath in a file name oidc.keystore.jwks.json\n");
+        System.out.println("{ \"keys\": [");
+        System.out.println(json);
+        System.out.println("]}");
+        System.out.println("\n");
+    }
 }
