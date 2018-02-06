f7d7b39e09afa3dd616a24361c798445e7ec664e
oharsta <oharsta@zilverline.com>
2018-02-06 10:54:43 +0100

    Added test assertions for the memberships and entitlements in the user info endpoint

--- oidc-server/src/test/java/oidc/openid/OIDCTestIntegration.java
+++ oidc-server/src/test/java/oidc/openid/OIDCTestIntegration.java
@@ -5,61 +5,63 @@ import org.junit.Test;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.util.List;
 import java.util.Map;
 
-import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static org.junit.Assert.assertEquals;
 
 public class OIDCTestIntegration extends AbstractTestIntegration {
 
-  private String scope = "openid profile email organization userids entitlement";
+    private String scope = "openid profile email organization userids entitlement";
 
-  @Test
-  @SuppressWarnings("unchecked")
-  public void testOpenIdCodeFlow() throws Exception {
-    Map body = doTestAuthorizationCode(scope);
-    String accessToken = (String) body.get("access_token");
+    @Test
+    @SuppressWarnings("unchecked")
+    public void testOpenIdCodeFlow() throws Exception {
+        Map body = doTestAuthorizationCode(scope);
+        String accessToken = (String) body.get("access_token");
 
-    String idToken = (String) body.get("id_token");
-    assertTokenId(idToken);
+        String idToken = (String) body.get("id_token");
+        assertTokenId(idToken);
 
-    //Call the userinfo endpoint which is allowed because of openid spec
-    HttpHeaders headers = getAuthorizationHeadersForUserInfo(accessToken);
-    Map<String, Object> userinfo = template.exchange(serverUrl + "/userinfo", HttpMethod.GET, new HttpEntity<>(headers), Map.class).getBody();
-    assertUserInfoResult(userinfo);
-  }
+        //Call the userinfo endpoint which is allowed because of openid spec
+        HttpHeaders headers = getAuthorizationHeadersForUserInfo(accessToken);
+        Map<String, Object> userinfo = template.exchange(serverUrl + "/userinfo", HttpMethod.GET, new HttpEntity<>
+            (headers), Map.class).getBody();
+        assertUserInfoResult(userinfo);
+    }
 
-  @Test
-  public void testOpenIdImplicitIdTokenFlow() throws Exception {
-    String fragment = doTestOAuthImplicitFlow(scope, "id_token");
-    assertTokenId(fragment);
-  }
+    @Test
+    public void testOpenIdImplicitIdTokenFlow() throws Exception {
+        String fragment = doTestOAuthImplicitFlow(scope, "id_token");
+        assertTokenId(fragment);
+    }
 
-  @Test(expected = HttpClientErrorException.class)
-  @SuppressWarnings("unchecked")
-  public void testOpenIdCodeFlowWithUnknownScope() throws Exception {
-    doTestAuthorizationCode(scope + " bogus");
-  }
+    @Test(expected = HttpClientErrorException.class)
+    @SuppressWarnings("unchecked")
+    public void testOpenIdCodeFlowWithUnknownScope() throws Exception {
+        doTestAuthorizationCode(scope + " bogus");
+    }
 
-  @Test
-  @SuppressWarnings("unchecked")
-  public void testClientCredentials() throws Exception {
-    String accessToken = doTestOAuthImplicitFlow("strange", "token", "https@//client.localhost.surfconext.nl");
+    @Test
+    @SuppressWarnings("unchecked")
+    public void testClientCredentials() throws Exception {
+        String accessToken = doTestOAuthImplicitFlow("strange", "token", "https@//client.localhost.surfconext.nl");
 
-    // Call the Introspect endpoint (e.g. impersonating a Resource Server) using the accessCode
-    String introspectUri = UriComponentsBuilder.fromHttpUrl(serverUrl + "/introspect")
-        .queryParam("token", accessToken)
-        .build().toUriString();
-    Map<String, Object> introspect = template.exchange(introspectUri, HttpMethod.GET, new HttpEntity<>(headersForTokenFetch), Map.class).getBody();
-      System.out.println(introspect);
-    assertEquals(true, introspect.get("active"));
-    String scope = (String) introspect.get("scope");
-    assertEquals("", scope);  
-  }
+        // Call the Introspect endpoint (e.g. impersonating a Resource Server) using the accessCode
+        String introspectUri = UriComponentsBuilder.fromHttpUrl(serverUrl + "/introspect")
+            .queryParam("token", accessToken)
+            .build().toUriString();
+        Map<String, Object> introspect = template.exchange(introspectUri, HttpMethod.GET, new HttpEntity<>
+            (headersForTokenFetch), Map.class).getBody();
+        System.out.println(introspect);
+        assertEquals(true, introspect.get("active"));
+        assertEquals(1, List.class.cast(introspect.get("memberships")).size());
+        assertEquals(2, List.class.cast(introspect.get("entitlements")).size());
+        String scope = (String) introspect.get("scope");
+        assertEquals("", scope);
+    }
 
 }
