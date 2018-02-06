c99e30512fe4e3e49e1595991d25aa8b3dd3da8e
oharsta <oharsta@zilverline.com>
2018-02-06 10:47:05 +0100

    Add isMemberof and Entitlements to the list of attributes - https://www.pivotaltracker.com/story/show/154954442

--- oidc-server/src/main/java/oidc/service/ExtendedIntrospectionResultAssembler.java
+++ oidc-server/src/main/java/oidc/service/ExtendedIntrospectionResultAssembler.java
@@ -5,10 +5,8 @@ import org.mitre.oauth2.model.OAuth2AccessTokenEntity;
 import org.mitre.oauth2.service.impl.DefaultIntrospectionResultAssembler;
 import org.mitre.openid.connect.model.UserInfo;
 import org.springframework.context.annotation.Primary;
-import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.stereotype.Service;
 
-import java.io.Serializable;
 import java.util.Map;
 import java.util.Set;
 
@@ -16,16 +14,19 @@ import java.util.Set;
 @Primary
 public class ExtendedIntrospectionResultAssembler extends DefaultIntrospectionResultAssembler {
 
-  @Override
-  public Map<String, Object> assembleFrom(OAuth2AccessTokenEntity accessToken, UserInfo userInfo, Set<String> authScopes) {
-    Map<String, Object> result = super.assembleFrom(accessToken, userInfo, authScopes);
-    if (userInfo != null && userInfo instanceof FederatedUserInfo) {
-      FederatedUserInfo federatedUserInfo = (FederatedUserInfo) userInfo;
-      result.put("schac_home", federatedUserInfo.getSchacHomeOrganization());
-      result.put("unspecified_id", federatedUserInfo.getUnspecifiedNameId());
-      result.put("authenticating_authority", federatedUserInfo.getAuthenticatingAuthority());
-      result.put("edu_person_principal_name", federatedUserInfo.getEduPersonPrincipalName());
+    @Override
+    public Map<String, Object> assembleFrom(OAuth2AccessTokenEntity accessToken, UserInfo userInfo, Set<String>
+        authScopes) {
+        Map<String, Object> result = super.assembleFrom(accessToken, userInfo, authScopes);
+        if (userInfo != null && userInfo instanceof FederatedUserInfo) {
+            FederatedUserInfo federatedUserInfo = (FederatedUserInfo) userInfo;
+            result.put("schac_home", federatedUserInfo.getSchacHomeOrganization());
+            result.put("unspecified_id", federatedUserInfo.getUnspecifiedNameId());
+            result.put("authenticating_authority", federatedUserInfo.getAuthenticatingAuthority());
+            result.put("edu_person_principal_name", federatedUserInfo.getEduPersonPrincipalName());
+            result.put("entitlements", federatedUserInfo.getEduPersonEntitlements());
+            result.put("memberships", federatedUserInfo.getIsMemberOfs());
+        }
+        return result;
     }
-    return result;
-  }
 }
