eafc15acdeee1983472c9b04eb8ccf5c090225cf
oharsta <oharsta@zilverline.com>
2018-02-07 15:40:15 +0100

    Small refactor

--- oidc-server/src/main/java/oidc/saml/ExtendedWebSSOProfileOptions.java
+++ oidc-server/src/main/java/oidc/saml/ExtendedWebSSOProfileOptions.java
@@ -12,7 +12,20 @@ public class ExtendedWebSSOProfileOptions extends WebSSOProfileOptions {
     private String idpSingleSignOn;
 
     public ExtendedWebSSOProfileOptions(WebSSOProfileOptions options, String idpSingleSignOn) {
-        this.options = options;
+        setBinding(options.getBinding());
+        setPassive(options.getPassive());
+        setForceAuthN(options.getForceAuthN());
+        setIncludeScoping(options.isIncludeScoping());
+        setProxyCount(options.getProxyCount());
+        setAuthnContexts(options.getAuthnContexts());
+        setNameID(options.getNameID());
+        setAllowCreate(options.isAllowCreate());
+        setAuthnContextComparison(options.getAuthnContextComparison());
+        setAllowedIDPs(options.getAllowedIDPs());
+        setProviderName(options.getProviderName());
+        setAssertionConsumerIndex(options.getAssertionConsumerIndex());
+        setRelayState(options.getRelayState());
+        setRequesterIds(options.getRequesterIds());
         this.idpSingleSignOn = idpSingleSignOn;
     }
 
@@ -20,73 +33,4 @@ public class ExtendedWebSSOProfileOptions extends WebSSOProfileOptions {
         return idpSingleSignOn;
     }
 
-    @Override
-    public String getBinding() {
-        return this.options.getBinding();
-    }
-
-    @Override
-    public Boolean getPassive() {
-        return this.options.getPassive();
-    }
-
-    @Override
-    public Boolean getForceAuthN() {
-        return this.options.getForceAuthN();
-    }
-
-    @Override
-    public Boolean isIncludeScoping() {
-        return this.options.isIncludeScoping();
-    }
-
-    @Override
-    public Integer getProxyCount() {
-        return this.options.getProxyCount();
-    }
-
-    @Override
-    public Collection<String> getAuthnContexts() {
-        return this.options.getAuthnContexts();
-    }
-
-    @Override
-    public String getNameID() {
-        return this.options.getNameID();
-    }
-
-    @Override
-    public Boolean isAllowCreate() {
-        return this.options.isAllowCreate();
-    }
-
-    @Override
-    public AuthnContextComparisonTypeEnumeration getAuthnContextComparison() {
-        return this.options.getAuthnContextComparison();
-    }
-
-    @Override
-    public Set<String> getAllowedIDPs() {
-        return this.options.getAllowedIDPs();
-    }
-
-    @Override
-    public String getProviderName() {
-        return this.options.getProviderName();
-    }
-
-    @Override
-    public Integer getAssertionConsumerIndex() {
-        return this.options.getAssertionConsumerIndex();
-    }
-
-    @Override
-    public String getRelayState() {
-        return this.options.getRelayState();
-    }
-
-    @Override
-    public Set<String> getRequesterIds() {
-        return this.options.getRequesterIds();
-    }
 }
